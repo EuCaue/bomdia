080bff977b436f960ab9ca699577d9970570188b
Caue <caueadriano.castro@gmail.com>
2021-04-15 21:58:42 -0300

    aaaaaaaaaaa

--- cursoemvideo/aula14ex/ex016/script.js
+++ cursoemvideo/aula14ex/ex016/script.js
@@ -5,15 +5,30 @@ function contar() {
     let res = window.document.querySelector('div#res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Valores Inválidos!')
+        res.innerHTML = ('Impossível contar!')
+        // alert('[ERRO] Valores Inválidos!')
+        
     }else{
         res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p){
-           res.innerHTML += ` ${c} \u{1F449}` 
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f){
+            // CONTAGEM CRESCENTE
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}` 
+             }
+             
+        }else{
+            // CONTAGEM REGRESSIVA
+            for(let c = i; c>= f; c-= p){
+                res.innerHTML += ` ${c} \u{1F499}`
+            }
+            
         }
         res.innerHTML += `\u{1F3C1}`
     }
