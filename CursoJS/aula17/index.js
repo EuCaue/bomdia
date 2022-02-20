3ba9565b4a0c1af6d46c7285e72b944f1165c7ab
EuCaue <caueadriano.castro@gmail.com>
2022-02-20 16:33:27 -0300

    funções :)

---
+++ CursoJS/aula17/index.js
@@ -0,0 +1,28 @@
+/*function saudacao(nome) {
+    return `Bom dia, ${nome}!`;
+}
+
+const variavel  = saudacao('Cauê');
+console.log(variavel);*/
+// depois de achar o return, não será mais executado. 
+// se não for passado parametro ou passado somente um, será considero NaN. 
+// 
+
+/* function soma(x = 1 , y = 1) {
+    const resultado = x + y 
+    return resultado
+}
+
+const resultado = soma(2, 2);
+console.log(resultado); */ 
+
+/* const raiz = function (n) {
+    return n ** 0.5 
+};
+
+console.log(raiz(16)); */
+
+const raiz = n => n ** 0.5 
+
+
+console.log(raiz(16));
