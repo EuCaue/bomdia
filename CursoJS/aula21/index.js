25e9601b2966ecb7dea5a587c2947f83b828b05d
EuCaue <caueadriano.castro@gmail.com>
2022-02-22 12:47:26 -0300

    aula 21 & 22

---
+++ CursoJS/aula21/index.js
@@ -0,0 +1,16 @@
+/* 
+    OPERADORES DE COMPARAÇÃO 
+> Maiorque
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== igualdade (valor) *********
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) **********
+!== diferente estrita (valor e tipo)
+*/
+const num1 = 10 // number
+const num2 = '10' // string
+const comp = num1 === num2;
+console.log(comp);
+
---
+++ CursoJS/aula22/index.js
@@ -0,0 +1,16 @@
+/* 
+Operadores Lógicos
+&& -> AND -> E // Todas as expressões precisam ser verdadeiras para retornar true.
+|| -> OR -> OU // Precisa de somente uma expressão para ser true
+! -> NOT -> NÃO // Nega ou inverte a 
+*/
+// const expressaoAnd = true && true && false && true;
+// const expressaoOr = true || false || false || false;
+
+// const usuario = 'Cauê'; // form usuário digitou
+// const senha = '123456'; // form usuário digitou
+// //                            true                  true
+// const vailogar = usuario === 'Cauê' && senha === '123456';
+// console.log(vailogar );
+
+console.log(!true);
