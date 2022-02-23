b039e327a26f98a1cdce2eb6e89f42d70001e82a
EuCaue <caueadriano.castro@gmail.com>
2022-02-23 11:55:18 -0300

    aula24

---
+++ CursoJS/aula24/index.js
@@ -0,0 +1,29 @@
+// Entre 0 - 11  - Bom dia
+// Entre 12 - 17 - Boa Tarde
+// Entre 18 - 23 - Boa Noite
+
+// if pode ser usado sozinho
+// else if não pode ser executado sozinho
+// Else if, pode ter quantos eu quiser. 
+// Pode só ter um else, na checagem.
+// Podemos usar condições sem else if, utilizando apenas if e else
+
+
+// const hora = 123;
+// 
+// if (hora >= 0 && hora <= 11){
+//     console.log('Bom dia!');
+// } else if (hora >= 12 && hora <= 15) {
+//     console.log('Boa Tarde');
+// } else if (hora >= 18 && hora <= 23) {
+//     console.log('Boa Noite');
+// } else {
+//     console.log('Olá!');
+// }
+
+const tenhoGrana = true;
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+}else {
+    console.log('Não vou sair de casa');
+}
