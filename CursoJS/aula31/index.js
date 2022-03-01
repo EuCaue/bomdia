fe1dc5a66e7f4290b97bef5c90e4aba94ae22fa3
EuCaue <caueadriano.castro@gmail.com>
2022-03-01 15:19:10 -0300

    aula31

---
+++ CursoJS/aula31/index.js
@@ -0,0 +1,30 @@
+// const verdadeira = true;
+// 
+// // let tem escopo de bloco { ...bloco}
+// // var só tem escopo de função 
+// 
+// let nome = 'Cauê'; // criando
+// var nome2 = 'Cauê';
+// 
+// var nome2 = 'Souza';  // redeclarada
+// 
+// 
+// if (verdadeira) {
+//     let nome = 'Souza'; // criando
+//     var nome2 = 'Souza';  // redeclarada
+// 
+//     console.log(nome, nome2);
+// 
+//     if (verdadeira) {
+//         let nome = 'Outra coisa';
+//         console.log(nome, nome2);
+//     }
+// }
+
+// function falaOi () {
+//     var nome = 'Luiz';
+// }
+// 
+// console.log(nome); // erro 
+// falaOi();
+
