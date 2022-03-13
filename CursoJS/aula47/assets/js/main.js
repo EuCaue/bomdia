159752358e78dd5a6b9a76ae9f29e7eb976b9112
EuCaue <caueadriano.castro@gmail.com>
2022-03-13 03:32:19 -0300

    exercício timer conclido

--- CursoJS/aula47/assets/js/main.js
+++ CursoJS/aula47/assets/js/main.js
@@ -1,3 +1,4 @@
+// Estrutura geral, o relógio zerado e formatado.
 function getTimeFromSeconds (seconds) {
     const date = new Date(seconds * 1000)
     return date.toLocaleTimeString('pt-BR', {
@@ -5,7 +6,7 @@ function getTimeFromSeconds (seconds) {
         timeZone: 'UTC',
     });
 }
-
+// Varíaveis para funcionar
 const watch = document.querySelector('.watch');
 const start = document.querySelector('.start');
 const pause = document.querySelector('.pause');
@@ -13,18 +14,32 @@ const clear =  document.querySelector('.clear');
 let seconds = 0; 
 let timer;
 
+// começar o timer
 function startWatch () {
+    
     timer = setInterval(() => {
+        watch.style.color = '';
         seconds++;
         watch.innerHTML = getTimeFromSeconds(seconds);
     }, 1000);
 }
-
-function stopWatch () { 
-    const stopTimer = setTimeout(() => {
+// pausa o timer
+function pauseWatch () { 
+    const pauseTimer = setTimeout(() => {
         clearInterval(timer)
-        
+        watch.style.color = 'red'; 
     }, 0);
+}
+
+// limpar o timer.
+function stopWatch() {
+    const stopTimer = setTimeout(() => {
+        watch.style.color = '';
+        clearInterval(timer);
+        watch.innerHTML = '00:00:00'
+    }, 0);
+    
+    
 }
 
 start.addEventListener('click', (e) => {
@@ -32,10 +47,10 @@ start.addEventListener('click', (e) => {
 });
 
 pause.addEventListener('click', (e) => {
-    stopWatch();
+    pauseWatch();
 });
 
 clear.addEventListener('click', (e) => {
-    alert('Cliquei no Parar')
+    stopWatch();
 });
 
